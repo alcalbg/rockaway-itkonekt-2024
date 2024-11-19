6a2f4b91471b92c453ce211074657f2d12eff028
Dylan Beattie <dylan@dylanbeattie.net>
2024-11-19 14:04:45 +0000

    Demos for minimal APIs and Razor Pages!

---
+++ demos/AspNetRazor/Pages/People.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetRazor.Pages;
+
+public class PeopleModel : PageModel
+{
+	public List<string> Names { get;set; } = [
+		"John", "Paul", "George", "Ringo" ];
+
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PeopleModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
+
