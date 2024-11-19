ea3f70d040f3f1cd1f610b98a780413a1a88b1ee
Dylan Beattie <dylan@dylanbeattie.net>
2024-11-19 15:31:51 +0000

    Demonstration of replacing service with a test/stub/fake in the context of a specific test

--- Rockaway/Rockaway.WebApp.Tests/Services/StatusTests.cs
+++ Rockaway/Rockaway.WebApp.Tests/Services/StatusTests.cs
@@ -1,17 +1,32 @@
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 using Rockaway.WebApp.Services;
 
 namespace Rockaway.WebApp.Tests.Services;
 
 public class StatusTests {
 
+	private static readonly ServerStatus testStatus = new() {
+		Assembly = "TEST_ASSEMBLY",
+		Modified = new DateTimeOffset(2021, 2, 3, 4, 5, 6, TimeSpan.Zero).ToString("O"),
+		Hostname = "TEST_HOST",
+		DateTime = new DateTimeOffset(2022, 3, 4, 5, 6, 7, TimeSpan.Zero).ToString("O")
+	};
+
+	private class TestStatusReporter : IStatusReporter {
+		public ServerStatus GetStatus() => testStatus;
+	}
+
 	private static readonly JsonSerializerOptions jsonSerializerOptions
 		= new(JsonSerializerDefaults.Web);
 
 	[Fact]
 	public async Task Status_Endpoint_Returns_Success() {
-		await using var factory = new WebApplicationFactory<Program>();
+		await using var factory = new WebApplicationFactory<Program>()
+			.WithWebHostBuilder(builder => builder.ConfigureServices(services => {
+				services.AddTransient<IStatusReporter, TestStatusReporter>();
+			}));
 		using var client = factory.CreateClient();
 		using var response = await client.GetAsync("/status");
 		response.EnsureSuccessStatusCode();
@@ -19,11 +34,15 @@ public class StatusTests {
 
 	[Fact]
 	public async Task Status_Endpoint_Returns_Hostname() {
-		await using var factory = new WebApplicationFactory<Program>();
+		await using var factory = new WebApplicationFactory<Program>()
+			.WithWebHostBuilder(builder => builder.ConfigureServices(services => {
+				services.AddTransient<IStatusReporter, TestStatusReporter>();
+			}));
 		using var client = factory.CreateClient();
 		var json = await client.GetStringAsync("/status");
 		var status = JsonSerializer.Deserialize<ServerStatus>(json, jsonSerializerOptions);
 		Assert.NotNull(status);
-		Assert.Equal("HAYWORTH", status.Hostname);
+		// WORKS ON MY MACHINE!
+		Assert.Equal(testStatus.Hostname, status.Hostname);
 	}
 }
